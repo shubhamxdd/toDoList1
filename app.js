ab32d31d39cf182cdbdd8deec9c68d35eb2dc676
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2022-10-08 16:06:12 +0530

    final

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
+
+// console.log(date());
 
 const app = express();
 
@@ -12,13 +15,10 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 
 app.get("/", function (req, res) {
-    var today = new Date();
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
-    var day = today.toLocaleDateString("en-US", options)
+
+    let day = date.getDate();
+    // let day = date.getDay();
+
     res.render("list", { listTitle: day, newItems: tasks });
 });
 
